2a3124f0761ac92acbcc4e63a32a8c79649dc7a8
Tin Geber <tingeber@gmail.com>
2025-11-20 12:44:05 +0100

    formatter and linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
-import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    // @ts-expect-error
+    // TODO #1 - remove expect error when Astro updates to Vite 7
+    // https://github.com/withastro/astro/issues/14030#issuecomment-3027129338
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react(), sitemap(), mdx()],
   adapter: netlify(),
--- src/pages/markdown-page.md
+++ src/pages/markdown-page.md
@@ -4,12 +4,12 @@ layout: ../layouts/main.astro
 ---
 
 <div class="grid place-items-center h-screen content-center">
- <div class="py-2 px-4 bg-purple-500 text-white font-semibold rounded-lg shadow-md">
+ <div class="px-4 py-2  bg-purple-500  font-semibold text-white rounded-lg shadow-md">
   Tailwind classes also work in Markdown!
  </div>
  <a
   href="/"
-  class="p-4 underline hover:text-purple-500 transition-colors ease-in-out duration-200"
+  class="underline mb-2  hover:text-purple-500 transition-colors ease-in-out duration-200"
  >
   Go home
  </a>
