36909f4c29ca4996177f8df962481fd5b571119d
kjacks <kirkgjackson@gmail.com>
2025-11-24 14:46:00 +0100

    Added typography css settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,21 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.bunny(),
+        name: 'Inter',
+        weights: [100, 200, 300, 400, 500, 600],
+        cssVariable: '--font-inter',
+      }
+    ],
+  },
+
   vite: {
     // @ts-expect-error
     // TODO #1 - remove expect error when Astro updates to Vite 7
