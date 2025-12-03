232948e52dfc834cb8c3226a997bb170650d2c7f
kjacks <kirkgjackson@gmail.com>
2025-12-03 12:03:08 +0100

    Adapted renderMarkdownWIthImages to use getImage

--- src/utils/renderMarkdownWithImages.ts
+++ src/utils/renderMarkdownWithImages.ts
@@ -3,37 +3,49 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { visit } from 'unist-util-visit';
 import rehypeStringify from 'rehype-stringify';
+import { getImage } from 'astro:assets';
 
+const processImageNodes = () => async (tree: any) => {
+  // Find all image nodes in the tree
+  const imageNodes: any[] = [];
+  visit(tree, 'element', (node) => {
+    if (node.tagName === 'img') {
+      imageNodes.push(node);
+    }
+  });
 
-export const IMAGES = import.meta.glob('/src/assets/**/*.{png,jpg,jpeg,webp,avif}', {
-  eager: true,
-  import: 'default',
-});
+  // fetch images using Astro getImage
+  const imagePromises = imageNodes.map((node) => {
+    const src = node.properties?.src;
+    const alt = node.properties?.alt ?? '';
 
-
-export async function renderMarkdownWithImages(markdown: string) {
-  const html = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(() => (tree: any) => {
-      visit(tree, 'element', (node) => {
-        if (node.tagName === 'img') {
-          const src = node.properties?.src;
-          const alt = node.properties?.alt ?? '';
-
-          const image = IMAGES[src];
-
-          if (image) {
+    return new Promise((resolve) => {
+      getImage({ src })
+        .then((fetchedImage) => {
+          if (fetchedImage) {
             node.properties = {
-              ...image,
+              ...fetchedImage.attributes,
+              src: fetchedImage.src,
               alt,
             };
           }
-        }
+          resolve();
       });
-    })
+    });
+  })
+
+  await Promise.all(imagePromises);
+}
+
+const renderMarkdownWithImages = async (markdown: string) => {
+  const html = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(processImageNodes)
     .use(rehypeStringify, { allowDangerousHtml: true })
     .process(markdown);
 
     return String(html);
 }
+
+export default renderMarkdownWithImages;
